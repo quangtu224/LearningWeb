f3bfd172724ba7f6b2ac01566a212909ed2b9220
Quang Tu Dinh <quangtu224@gmail.com>
2026-09-23 16:57:40 +0200

    Adding new button and add event to store search history when press submit or enter keypress

--- JS/37_RandomColorBackGround/MiniProject4/main.js
+++ JS/37_RandomColorBackGround/MiniProject4/main.js
@@ -1,8 +1,26 @@
 const search = document.querySelector(".search");
 const button = document.querySelector(".btn");
-const input = document.querySelector(".input");
-
+const input = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const history = document.querySelector(".history");
 button.addEventListener("click", () => {
   search.classList.toggle("active");
   input.focus();
+});
+submit.addEventListener("click", () => {
+  const value = input.value;
+  const li = document.createElement("li");
+  li.innerText = value;
+  history.appendChild(li);
+  input.value = "";
+});
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    const value = input.value;
+    const li = document.createElement("li");
+    li.innerText = value;
+    history.appendChild(li);
+    input.value = "";
+  }
 });
